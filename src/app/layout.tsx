43b5f86ae6b44847857746bdf18b54cce4cb4980
Arthur Manenti <arthur.manenti.videira@gmail.com>
2026-03-07 19:54:38 -0300

    Lidando com metadados

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,25 @@
 import Header from "@/components/header";
 import "./globals.css";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Home Next JS do zero",
+  description: "Aprendendo Next JS do zero com o sujeito programador",
+  openGraph: {
+    title: "Aprendendo Next JS com o sujeito programador no Youtube",
+    description: "Aprendendo Next JS do zero com o sujeito programador",
+    images: ["https://github.com/noname697.png"],
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: true,
+    },
+  },
+};
 
 export default function RootLayout({
   children,
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,30 @@
-const Home = () => {
-return (
-  <div>
-    <h1>Olá Mundo!</h1>
-  </div>
-)
-}
+import { Metadata } from "next";
 
-export default Home
+export const metadata: Metadata = {
+  title: "Home - Aula de Next JS do zero!",
+  description: "Aprendendo Next JS do zero com o sujeito programador",
+  openGraph: {
+    title: "Aprendendo Next JS com o sujeito programador no Youtube",
+    description: "Aprendendo Next JS do zero com o sujeito programador",
+    images: ["https://github.com/noname697.png"],
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: true,
+    },
+  },
+};
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Olá Mundo!</h1>
+    </div>
+  );
+};
+
+export default Home;
