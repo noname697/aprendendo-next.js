569a84738d355336c12d8201fccfb0deeaf45843
Arthur Manenti <arthur.manenti.videira@gmail.com>
2026-03-07 19:34:58 -0300

    Roteamento entre páginas

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,8 +1,26 @@
+import Link from "next/link";
+
 const Header = () => {
   return (
-    <div>
-      <h1>Header</h1>
-    </div>
+    <header className="flex px-2 py-4 bg-zinc-900 text-white">
+      <div className="flex items-center justify-between w-full mx-auto max-w-7x1">
+        <div>NextJS</div>
+      </div>
+
+      <nav>
+        <ul className="flex items-center justify-center gap-2">
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/posts">Posts</Link>
+          </li>
+          <li>
+            <Link href="/dashboard">Dashboard</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 };
 
